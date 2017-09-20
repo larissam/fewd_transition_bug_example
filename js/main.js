3f744707506b2212ac44c478aad381e7a67b42a3
Instructor <instructor@larissasmacbook.gateway.sonic.net>
2017-09-20 00:55:55 -0700

    first

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// PART 3
+
+// WHEN the user clicks the button
+// Replace 'yourSelectorHere' with your button selector
+// Replace 'eventHere' with your event
+$('button').on('click', function() {
+    // Add the animate class to the bugs!
+    $('.bug').addClass('animate');
+
+});
